525b03e0783322795e1506fa7ded34cb0ba322b0
Vinicius Trecenti <vinitrecenti@gmail.com>
2024-10-16 23:57:24 -0300

    Learning about service and how to inject this in controller

--- src/main/java/com/example/first_spring_app/controller/HelloWorldController.java
+++ src/main/java/com/example/first_spring_app/controller/HelloWorldController.java
@@ -1,16 +1,26 @@
 package com.example.first_spring_app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.first_spring_app.service.HelloWorldService;
 
 @RestController
 @RequestMapping("/hello-world")
 public class HelloWorldController {
 
+    @Autowired //indica que precisa injentar a dependencia
+    private HelloWorldService helloWorldService;
+
+    // public HelloWorldController(HelloWorldService helloWorldService) {
+    //     this.helloWorldService = helloWorldService;
+    // }
     //indica que responde a requisicoes do tipo GET
     @GetMapping
     public String helloWorld(){
-        return "Hello World";
+        // return "Hello World";
+        return helloWorldService.helloWorld("Vinicius");
     }
 }
